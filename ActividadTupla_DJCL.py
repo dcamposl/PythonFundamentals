a4eedeee0a24dad5dd0eb7ef2ba8f2a96a2c7599
dcamposl <123569070+dcamposl@users.noreply.github.com>
2023-02-11 13:57:09 -0600

    Add files via upload

---
+++ ActividadTupla_DJCL.py
@@ -0,0 +1,19 @@
+##Actividad : Tupla
+## Delia Jeanette Campos López
+## 1.- Crea una tupla con una longitud de 5 usando diferentes tipos de datos.
+
+valores = ("Mexico",False,"Sol",3456, 4.56)
+print("Esto es una tupla:", valores)
+
+#2.- Cambiar dicha tupla a una lista.
+lista = list(valores)
+print("Esto es una lista:", lista)
+
+#3.- Crea un diccionario donde la clave sea del 1 al 5 y los elementos los datos de la lista.
+
+diccionario ={"Mexico":1,
+             False:2,
+             "Sol":3,
+             3456:4,
+             4.56:5}
+print("Esto es un diccionario", diccionario)
