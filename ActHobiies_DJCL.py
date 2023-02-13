3226332a3662ef6db0b50aaf871f6510f9d85fe3
dcamposl <123569070+dcamposl@users.noreply.github.com>
2023-02-13 16:44:13 -0600

    Add files via upload

---
+++ ActHobiies_DJCL.py
@@ -0,0 +1,30 @@
+#Actividad Hobbies
+#Delia Campos López
+
+
+#Algoritmo Básico
+#Algoritmo para mi hobbie: sacar a pasear a mi perrito Oliver
+
+#1.- Llamar a Oliver
+#2.- Ponerle su collar y su correa
+#3.- Tomar una bolsita para levantar sus desechos
+#4.- Tomar las llaves
+#5.- Salir de la casa
+#6.- Caminar al parque
+
+
+
+#Algoritmo Avanzado
+#Algoritmo para un proceso de captura de actividades
+
+#1.- Abrir la aplicación
+#2.- Hacer clik en el boton: "agregar actividad"
+#3.- Seleccionar el día, la hora y duración de la actividad
+#4.- Elegir el cliente 
+#5.- Elegir el proyecto
+#6.- Elegir la actividad
+#7.- Llenar la descripcion
+#8.- Clik en el boton: "guardar"
+#9.- Clik en el boton: "cerrar"
+#10.- Repetir los pasos de la 1 al 9 hasta completar los días a capturar
+#11.- Cerrar aplicación
